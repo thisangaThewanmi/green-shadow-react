e6ff1323605f12dba96d8c01be6ebcc60f33da71
thisangaThewanmi <thisangat2005@gmail.com>
2025-01-19 18:40:22 +0530

    created the structure of the Equipment Page

--- src/Vehicle.tsx
+++ src/Vehicle.tsx
@@ -113,37 +113,6 @@ function Vehicle(){
 
                     </main>
 
-
-                    {/*<div className='w-[95] ml-2 mr-2 flex-col'>*/}
-                    {/*    <Form/>*/}
-                    {/*    <Table>*/}
-                    {/*        <TableCaption>A list of your recent invoices.</TableCaption>*/}
-                    {/*        <TableHeader>*/}
-                    {/*            <TableRow>*/}
-                    {/*                <TableHead className="w-[100px]">Invoice</TableHead>*/}
-                    {/*                <TableHead>Status</TableHead>*/}
-                    {/*                <TableHead>Method</TableHead>*/}
-                    {/*                <TableHead className="text-right">Amount</TableHead>*/}
-                    {/*            </TableRow>*/}
-                    {/*        </TableHeader>*/}
-                    {/*        <TableBody>*/}
-                    {/*            {invoices.map((invoice) => (*/}
-                    {/*                <TableRow key={invoice.invoice}>*/}
-                    {/*                    <TableCell className="font-medium">{invoice.invoice}</TableCell>*/}
-                    {/*                    <TableCell>{invoice.paymentStatus}</TableCell>*/}
-                    {/*                    <TableCell>{invoice.paymentMethod}</TableCell>*/}
-                    {/*                    <TableCell className="text-right">{invoice.totalAmount}</TableCell>*/}
-                    {/*                </TableRow>*/}
-                    {/*            ))}*/}
-                    {/*        </TableBody>*/}
-                    {/*        <TableFooter>*/}
-                    {/*            <TableRow>*/}
-                    {/*                <TableCell colSpan={3}>Total</TableCell>*/}
-                    {/*                <TableCell className="text-right">$2,500.00</TableCell>*/}
-                    {/*            </TableRow>*/}
-                    {/*        </TableFooter>*/}
-                    {/*    </Table>*/}
-                    {/*</div>*/}
                 </div>
             </div>
             )
